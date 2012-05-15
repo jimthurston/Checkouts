387cfd60fc97ad7f55c39392d324a6b451f42000
Jim Thurston <jimthurston@gmail.com>
2012-05-15 21:37:40 +0100

    Re-format js

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,13 +1,9 @@
 // the app method accepts a fn to invoke on init unobtrusively 
-var run = function(application)
-{
-    if (navigator.userAgent.indexOf('Browzr') > -1)
-	{
+var run = function(application) {
+    if (navigator.userAgent.indexOf('Browzr') > -1) {
         // blackberry
         setTimeout(application, 250)	
-    }
-	else
-	{
+    } else {
         // attach to deviceready event, which is fired when phonegap is all good to go.
         x$(document).on('deviceready', application, false);
     }
@@ -17,45 +13,30 @@ var run = function(application)
 , store = new Lawnchair({adaptor:'dom'})
 
 // shows id passed
-, display = function(id)
-{
-    x$(["#welcome", "#settings", "#stats", "#gamestart", "#game"]).each
-	(
-		function(e, i)
-		{
-			var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
-			x$(e).css({ 'display':display })
-		}
-	);
+, display = function(id) {
+//    x$(["#welcome", "#map", "#settings"]).each(function(e, i) {
+    x$(["#welcome", "#settings", "#stats", "#gamestart", "#game"]).each(function(e, i) {
+        var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
+        x$(e).css({ 'display':display })
+    });
 }
 
 // reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
-, when = function(id, callback)
-{	
-    x$(id + '_button').on
-	(
-		'touchstart',
-		function ()
-		{
-			alert('a button was clicked!');
-			
-			if (x$(id).length > 0)
-				display(id);
-			if (callback)
-				callback.call(this);
-
-			return false;
-		}
-	);
+, when = function(id, callback) {
+    x$(id + '_button').on('touchstart', function () {
+    	alert('a button was clicked!');
+        if (x$(id).length > 0)
+            display(id);
+        if (callback)
+            callback.call(this);
+		return false;
+    });
 }
 
 // gets the value of the setting from the ui
-, ui = function(setting)
-{
+, ui = function(setting) {
     var radio = x$('#settings_form')[0][setting];
-	
-    for (var i = 0, l = radio.length; i < l; i++)
-	{
+    for (var i = 0, l = radio.length; i < l; i++) {
         if (radio[i].checked)
             return radio[i].value;
     }
