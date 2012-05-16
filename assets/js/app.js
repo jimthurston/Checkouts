dc844f59606669eac38c88a9e755564bea7c4ba2
Jim Thurston <jimthurston@gmail.com>
2012-05-16 14:18:08 +0100

    remove welcome alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,13 @@
 run(function () {
     // immediately invoked on first run
     var init = (function () {
+	/*
         if (navigator.network.connection.type == Connection.NONE) {
             alert("No internet connection - we won't be able to show you any maps");
         } else {
             alert("We can reach Google - get ready for some awesome maps!");
         }
+	*/
     })();
     
     // a little inline controller
