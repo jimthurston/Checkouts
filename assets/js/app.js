77b41c6673403483c3d8ab1870647aedace67beb
Jim Thurston <jimthurston@gmail.com>
2012-05-17 11:34:36 +0100

    game table tidy

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,6 @@
 run(function () {
     // immediately invoked on first run
     var init = (function () {
-	/*
-        if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
-        } else {
-            alert("We can reach Google - get ready for some awesome maps!");
-        }
-	*/
     })();
     
     // a little inline controller
@@ -31,27 +24,6 @@ run(function () {
 			}
 		});
 	});
-	/*
-    when('#map', function () {
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
-
-                x$('img#static_map').attr('src', path);
-            }, function () {
-                x$('img#static_map').attr('src', "assets/img/gpsfailed.png");
-            });
-        });
-    });
-	*/
     when('#save', function () {
 	/*
         store.save({
