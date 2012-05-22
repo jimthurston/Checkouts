12c3ca504655f112170e82759c39064d2fe6492a
Jim Thurston <jimthurston@gmail.com>
2012-05-22 17:11:56 +0100

    game start settings show

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,7 @@ run(function() {
 		alert("trying to get game settings...");
 		// load game config from store and make sure we persist radio buttons.
 		
-		/*
+		
 		store.get('gameSettings', function(saved) {
 			if (saved) {
 				if (saved.minDifficulty) {
@@ -59,7 +59,7 @@ run(function() {
 				}
 			}
 		});
-		*/
+		
 	}
 	);
 	
